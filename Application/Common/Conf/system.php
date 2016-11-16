880b19022c4f0600aebe233f1db7e9b4fe9f12f3
xiaoxiao <nxxniubility@users.noreply.github.com>
2016-11-16 11:36:50 +0800

    Delete system.php

--- Application/Common/Conf/system.php
+++
@@ -1,9 +0,0 @@
-<?php 
-return array (
-  'ADMIN_SUPER_ROLE' => '5',
-  'ADMIN_MARKET_ROLE' => '10,3,28',
-  'ADMIN_EDUCATIONAL_ROLE' => '32',
-  'ADMIN_SERVICE_ROLE' => 2,
-  'ADMIN_USER_MAX_DISCOUNT' => '3000',
-  'SMSHINT_USER' => '160203',
-);
